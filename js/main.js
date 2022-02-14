76f37d4e860aea3c7d5c617f96703418d3b84925
MaryBayt <94868308+MaryBayt@users.noreply.github.com>
2022-02-14 16:53:53 +0300

    add mobile v and clicks for burger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const swiper = new Swiper('.main-slider', {
     // Optional parameters
-    slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
   
@@ -8,4 +7,24 @@ const swiper = new Swiper('.main-slider', {
     navigation: {
       nextEl: '.main-slider-arrow',
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+    }
   });
+
+  let burger = document.querySelector('.menu_burger');
+  let closeMenu = document.querySelector('.header_menu_close');
+  let menuMobile = document.querySelector('.header');
+
+  burger.addEventListener('click', () => {
+    menuMobile.style.display = 'block';
+  })
+
+  closeMenu.addEventListener('click', () => {
+    menuMobile.style.display = 'none';
+  })
